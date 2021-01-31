5d3b31041caa430f26a1708df367912d49cdd633
Catur Hidayat <c.meong@gmail.com>
2021-01-31 19:57:20 +0700

    Function dalam 1 file,  component dgn function

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Header from "./Header"
+
+const Footer = ()=>{
+  return(
+    <div>
+      <h2>Halaman Footer</h2>
+      <p>2021</p>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
         <h1>Render Element</h1>
+        <Footer/>
       </div>
     )
   }
