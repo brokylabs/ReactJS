ed1bc0311cd68dc5663e0bada789e72f0b69252c
Catur Hidayat <c.meong@gmail.com>
2021-01-31 13:33:53 +0700

    Menulis Component pada file terpisah

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,5 @@
 import React, { Component } from 'react';
-
-class Header extends Component {
-  render() {
-    return(
-      <div>
-        <h2>Makanan Khas Indonesia</h2>
-      </div>
-    )
-  }
-}
+import Header from "./Header"
 class App extends Component {
   render() {
     return (
