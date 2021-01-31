dc03ad3837319f36dc704c0b281533ff2352d348
Catur Hidayat <c.meong@gmail.com>
2021-01-31 20:07:20 +0700

    Penulisan Function Component

--- hello-react/src/Footer.js
+++ hello-react/src/Footer.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 const Footer = ()=>{
+  let name  = "NIOMIC"
     return(
       <div>
         <h2>Halaman Footer</h2>
-        <p>2021</p>
+        <p>2021. {name}</p> 
       </div>
     )
   }
+
+  // function Footer(){
+  //   return(
+  //     <div>
+  //       <h3>Halaman Footer Bro!</h3>
+  //       <p>2021</p>
+  //     </div>
+  //   )
+  // }
 
 
   export default Footer;
