7d4aa17b479bfe756b6fb43e84d7cc2c0cc58088
Catur Hidayat <c.meong@gmail.com>
2021-02-01 19:10:16 +0700

    Penulisan State

--- hello-react/src/component/Header.js
+++ hello-react/src/component/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      daftar : "Daftar makanan Nusantara"
+    };
+
+  }
     render() {
       return(
         <div>
           <h2>Makanan Khas Indonesia</h2>
+          <p>{this.state.daftar}</p>
         </div>
       )
     }
