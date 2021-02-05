1bd6f84a5cc29b30fdd62a8a16eebd24cecc1a01
ZoreIves <c.meong@gmail.com>
2021-02-05 14:17:30 +0700

    event onchange pada class component

--- hello-react/src/component/Main.js
+++ hello-react/src/component/Main.js
@@ -21,12 +21,24 @@ class Main extends Component{
         })
     }
 
+    ubahData(e){
+        // console.log(e.target.value);
+        
+        let nama = document.getElementById("ketikNama");
+        let inputValue = e.target.value;
+        let say = "Hallo : "
+
+        nama.innerHTML = say + inputValue;
+    }
+
     render(){
         return(
             <div>
                 <h2>{this.state.menuMakanan}</h2>
                 <h3>{this.state.menuMinuman}</h3>
-                <button onClick ={this.ubahMenu} href="/">Change</button><br></br>
+                <button onClick ={this.ubahMenu} href="/">Change</button><br></br><br></br>
+                <p id="ketikNama">Hallo : </p>
+                <input type="text" onChange={this.ubahData} placeholder="Ketik nama anda"></input>
             </div>
         )
     }
