39559284aa10349135fdf7e9707922d06accbc61
Catur Hidayat <c.meong@gmail.com>
2021-02-06 11:48:15 +0700

    Conditional Rendering

--- hello-react/src/component/Header.js
+++ hello-react/src/component/Header.js
@@ -5,7 +5,8 @@ class Header extends Component {
     super(props);
     this.state = {
       daftar : "Daftar makanan Nusantara",
-      dataList : this.props.List
+      dataList : this.props.List,
+      statusRender : false
     };
     this.handlePesan = this.handlePesan.bind(this)
   }
@@ -19,18 +20,30 @@ class Header extends Component {
   componentDidMount(){
     console.log("componentDidMount : run...");
   }
-
-    render() {
-      console.log("Render : run...");
-      return(
-        <div>
-          <h2>Makanan Khas Indonesia</h2>
-          <p>{this.state.daftar}</p>
-          <p>{this.state.dataList}</p>
-          <a href="/" onClick={(e)=>this.handlePesan("Pesan Header", e)}>Halaman Header!</a>
-        </div>
-      )
-    }
+  render() {
+    return(
+      <div>
+        {this.state.statusRender === true ? (
+          <h1>Selamat Datang</h1>
+        ): (
+          <h1>Selamat Tinggal</h1>
+        )}
+      </div>
+    )
   }
+}
+
+  //   render() {
+  //     console.log("Render : run...");
+  //     return(
+  //       <div>
+  //         <h2>Makanan Khas Indonesia</h2>
+  //         <p>{this.state.daftar}</p>
+  //         <p>{this.state.dataList}</p>
+  //         <a href="/" onClick={(e)=>this.handlePesan("Pesan Header", e)}>Halaman Header!</a>
+  //       </div>
+  //     )
+  //   }
+  // }
 
   export default Header;
