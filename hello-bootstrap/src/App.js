a683418ac5595748a4a8a3214d0ba6b83a4fdc26
Catur Hidayat <c.meong@gmail.com>
2021-02-08 05:14:37 +0700

    Bootstrap Layout dan Media

--- hello-bootstrap/src/App.js
+++ hello-bootstrap/src/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import { Button } from "react-bootstrap"
+import React, { Component } from 'react';
+import { Container, Row, Col, Media } from "react-bootstrap"
 
-function App() {
-  return (
-    <div>
-      <Button varian="primary">Button</Button>
-      <h2>Hello Bootstap</h2>
-    </div>
+class App extends Component {
+  render(){
+    return (
+    <Container>
+      <Row>
+        <Col>
+          <Media>
+            <img 
+            width={64}
+            height={64}
+            src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png" 
+            alt="Sate Klatak"
+            className="mr-3"></img>
+            <Media.Body>
+              <h5>Sate Klatak</h5>
+              <p>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
+              </p>
+            </Media.Body>
+          </Media>
+        </Col>
+        <Col>
+        <Media>
+        <img
+          width={64}
+          height={64}
+          className="mr-3"
+          src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+          alt="Generic placeholder"
+        />
+        <Media.Body>
+          <h5>Sate Klatak Juga</h5>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+            </p>
+        </Media.Body>
+        </Media>
+        </Col>
+      </Row>
+      <Row>
+      <Col>Col 1</Col>
+      <Col>Col 2</Col>
+      <Col>Col 3</Col>
+      </Row>
+    </Container>
   );
-}
+}}
 
 export default App;
