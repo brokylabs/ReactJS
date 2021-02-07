8f5c52a67ddf86b0d1e659e71b11a78ab66f9803
Catur Hidayat <c.meong@gmail.com>
2021-02-07 21:14:53 +0700

    React with Bootstrap

---
+++ hello-bootstrap/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from "react-bootstrap"
+
+function App() {
+  return (
+    <div>
+      <Button varian="primary">Button</Button>
+      <h2>Hello Bootstap</h2>
+    </div>
+  );
+}
+
+export default App;
