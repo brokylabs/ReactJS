c902f5cbed9c953d7677296af2c032b3fdf383fb
Catur Hidayat <c.meong@gmail.com>
2021-02-07 18:39:12 +0700

    Install React Route

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
+import App from './components/home/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
