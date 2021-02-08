a3820cfa5c4115603b776b177d77a935125457aa
Catur Hidayat <c.meong@gmail.com>
2021-02-09 05:17:25 +0700

    React Semantic-UI

---
+++ hello-semantic-ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "semantic-ui-css/semantic.min.css"
+import { Button } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello Semantic UI</h2>
+      <Button primary>Button Pertama</Button>
+      <Button secondary>Button Kedua</Button>
+    </div>
+  );
+}
+
+export default App;
