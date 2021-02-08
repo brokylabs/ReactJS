a38700a51d5736b435ec0ac20dd9392a6e40356a
Catur Hidayat <c.meong@gmail.com>
2021-02-09 05:35:42 +0700

    React Semantic-UI Container

--- hello-semantic-ui/src/App.js
+++ hello-semantic-ui/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import "semantic-ui-css/semantic.min.css"
-import { Grid, Button } from "semantic-ui-react";
+import { Grid, Button, Container } from "semantic-ui-react";
 
 function App() {
   return (
     <div>
-      
+      <Container textAlign="left">
+      MAMP: Alternatif XAMPP untuk pengguna MacOS – Buat Kanca IT yang sedang belajar web development pasti tidak asing dengan localhost atau local server yang berjalan di PC / Laptop kita. Beberapa dari kanca IT mungkin sudah banyak mendengar software XAMPP. Ya, XAMPP adalah software untuk local server yang sudah umum digunakan, terutama pengguna windows. Pada dasarnya XAMMP bersifat cross-platform, yang artinya bisa digunakan di Windows, Linux dan MacOS.
+      </Container>
+      <br></br>
+      <Container textAlign="right">
+      MAMP: Alternatif XAMPP untuk pengguna MacOS – Buat Kanca IT yang sedang belajar web development pasti tidak asing dengan localhost atau local server yang berjalan di PC / Laptop kita. Beberapa dari kanca IT mungkin sudah banyak mendengar software XAMPP. Ya, XAMPP adalah software untuk local server yang sudah umum digunakan, terutama pengguna windows. Pada dasarnya XAMMP bersifat cross-platform, yang artinya bisa digunakan di Windows, Linux dan MacOS.
+      </Container>
+      <br></br>
+      <Container textAlign="center">
+      MAMP: Alternatif XAMPP untuk pengguna MacOS – Buat Kanca IT yang sedang belajar web development pasti tidak asing dengan localhost atau local server yang berjalan di PC / Laptop kita. Beberapa dari kanca IT mungkin sudah banyak mendengar software XAMPP. Ya, XAMPP adalah software untuk local server yang sudah umum digunakan, terutama pengguna windows. Pada dasarnya XAMMP bersifat cross-platform, yang artinya bisa digunakan di Windows, Linux dan MacOS.
+      </Container>
+      <br></br>
+      <Container textAlign="justified">
+      MAMP: Alternatif XAMPP untuk pengguna MacOS – Buat Kanca IT yang sedang belajar web development pasti tidak asing dengan localhost atau local server yang berjalan di PC / Laptop kita. Beberapa dari kanca IT mungkin sudah banyak mendengar software XAMPP. Ya, XAMPP adalah software untuk local server yang sudah umum digunakan, terutama pengguna windows. Pada dasarnya XAMMP bersifat cross-platform, yang artinya bisa digunakan di Windows, Linux dan MacOS.
+      </Container>
+      <br></br>
       <Grid container columns={3}>
         <Grid.Column>
           <Button primary>Sign Up</Button>
@@ -15,6 +30,7 @@ function App() {
         <Grid.Column>2</Grid.Column>
         <Grid.Column>3</Grid.Column>
       </Grid>
+      
     </div>
   );
 }
