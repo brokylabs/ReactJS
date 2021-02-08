97439a668ee0c9f54e3a3a11da4cbda7d5fdb100
Catur Hidayat <c.meong@gmail.com>
2021-02-09 05:26:41 +0700

    React Semantic-UI Button

--- hello-semantic-ui/src/App.js
+++ hello-semantic-ui/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import "semantic-ui-css/semantic.min.css"
-import { Grid } from "semantic-ui-react";
+import { Grid, Button } from "semantic-ui-react";
 
 function App() {
   return (
     <div>
       
       <Grid container columns={3}>
-        <Grid.Column>1</Grid.Column>
+        <Grid.Column>
+          <Button primary>Sign Up</Button>
+          <Button secondary>Sign In</Button>
+          <Button basic color="green">Forgot Password</Button>
+        </Grid.Column>
         <Grid.Column>2</Grid.Column>
         <Grid.Column>3</Grid.Column>
       </Grid>
