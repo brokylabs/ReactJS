afd128f230b7ce8529ecfddbcc89a0e7a3da2815
ZoreIves <c.meong@gmail.com>
2021-02-10 14:11:26 +0700

    React materialize - Card

--- hello-materialize/src/App.js
+++ hello-materialize/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import { Button } from "react-materialize"
+import { Button, Card, CardTitle, Col, Row } from "react-materialize"
 
 class App extends Component {
   render (){
     return (
      <div>
-       <h2>Hello React Materialize</h2>
+       <h3>Hello React Materialize</h3>
   
        <Button>Login</Button>
+       <Row>
+         <Col m="4">
+            <Card className="green" title="Materialize Card" header={<CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">Fun</CardTitle>}>Tees</Card>
+         </Col>
+         <Col m="4">
+            <Card className="green" title="Materialize Card" header={<CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">Fun</CardTitle>}>Tees</Card>
+         </Col>
+         <Col m="4">
+            <Card className="green" title="Materialize Card" header={<CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">Fun</CardTitle>}>Tees</Card>
+         </Col>
+       </Row>
      </div>
     );
 
