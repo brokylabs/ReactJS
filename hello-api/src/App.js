675d948e4e0cba6a2bf0658782c2beb59a586a02
Catur Hidayat <c.meong@gmail.com>
2021-02-12 23:00:28 +0700

    React-api Interaksi-api PUT

--- hello-api/src/App.js
+++ hello-api/src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     super(props);
     this.state = {
       dataApi : [],
+      editData : false,
       dataPost : {
         id: 0,
         title: "",
@@ -21,6 +22,17 @@ class App extends Component {
       })
   }
 
+  clearInput = () => {
+    let newdataPost = {...this.state.dataPost}
+    newdataPost["id"] = ""
+    newdataPost["title"] = ""
+    newdataPost["body"] = ""
+
+    this.setState({
+      dataPost : newdataPost
+    })
+  }
+
   handleRemove = (e) => {
     console.log(e.target.value);
     fetch(`http://localhost:3005/posts/${e.target.value}`, {
@@ -28,26 +40,62 @@ class App extends Component {
     }).then(res => this.handleReload())
   }
 
-
   handleChange = (e) => {
     let newdataPost  = {...this.state.dataPost}
-    newdataPost['id'] = new Date().getTime()
+    if(this.editData === false){
+      newdataPost['id'] = new Date().getTime()
+    }
+
     newdataPost[e.target.name] = e.target.value
     this.setState({
       dataPost : newdataPost
     }, () => console.log(this.state.dataPost))
   }
 
+  getData = (e) => {
+    axios.get(`http://localhost:3005/posts/${e.target.value}`)
+      .then(res => {
+        this.setState({
+          dataPost : res.data,
+          editData : true
+        })
+      })
+  }
+
   postData = () => {
     // axios.post(`http://localhost:3005/posts`, this.state.dataPost)
     //   .then(res  => { this.handleReload()})
-    console.log("add data");
-    axios({
-      method : "POST",
-      url : "http://localhost:3005/posts",
-      data : this.state.dataPost
-    }).then(res  => { this.handleReload()})
+    // console.log("add data");
+
+    if(this.state.editData === false){
+      axios({
+        method : "POST",
+        url : "http://localhost:3005/posts",
+        data : this.state.dataPost
+      }).then(res  => { 
+        this.handleReload();
+
+        let newdataPost = {...this.state.dataPost}
+        newdataPost["id"] = ""
+        newdataPost["title"] = ""
+        newdataPost["body"] = ""
+        this.setState({
+          dataPost : newdataPost
+          // dataPost : this.clearInput()
+        })
+      })} else {
+        axios.put(`http://localhost:3005/posts/${this.state.dataPost.id}`, this.state.dataPost)
+          .then(res => {
+            this.handleReload()
+            this.clearInput()
+          }).then(() => {
+            this.setState({
+              editData : false
+            })
+          })
+      }     
   }
+
 
   componentDidMount(){
 
@@ -66,9 +114,10 @@ class App extends Component {
     return (
       <div>
         <h3>React - API</h3>
-          <input type="text" name="title" placeholder="Masukan Title" onChange={this.handleChange}></input>
-          <input type="text" name="body" placeholder="Masukan Body" onChange={this.handleChange}></input>
-          <button type="submit" onClick={this.postData}>Add Data</button>
+          <input type="text" name="title" value={this.state.dataPost.title} placeholder="Masukan Title" onChange={this.handleChange}></input>
+          <input type="text" name="body" value={this.state.dataPost.body} placeholder="Masukan Body" onChange={this.handleChange}></input>
+          <button type="submit" onClick={this.postData}>Save Data</button>
+          
         {this.state.dataApi.map((dat, index)=>{
           return (
             <div key={index}>
@@ -76,6 +125,7 @@ class App extends Component {
                   <p>{dat.title}</p>
                   <p>{dat.body}</p>
                   <button value={dat.id} onClick={this.handleRemove}>Hapus Item</button>
+                  <button value={dat.id} onClick={this.getData}>Edit Data</button>
             </div>
           )
         })}
