cbc5037b192199e45ce3dd27df6ef90ec4100c52
Catur Hidayat <c.meong@gmail.com>
2021-02-12 17:54:18 +0700

    React-api Interaksi-api POST

--- hello-api/src/App.js
+++ hello-api/src/App.js
@@ -4,30 +4,60 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      dataApi : []
+      dataApi : [],
+      dataPost : {
+        id: 0,
+        title: "",
+        body : ""
+      }
     }
-    this.handleRemove = this.handleRemove.bind(this)
+    // this.handleRemove = this.handleRemove.bind(this)
   }
 
-  handleReload(){
+  handleReload = () => {
     axios.get("http://localhost:3005/posts").then(res => {
         this.setState({ dataApi : res.data })
         // console.log(res.data);
       })
   }
 
-  handleRemove(e){
+  handleRemove = (e) => {
     console.log(e.target.value);
-    fetch(`http://localhost:3005/posts/${e.target.value}`, {method : "DELETE" }).then(res => this.handleReload())
+    fetch(`http://localhost:3005/posts/${e.target.value}`, {
+      method : "DELETE" 
+    }).then(res => this.handleReload())
+  }
+
+
+  handleChange = (e) => {
+    let newdataPost  = {...this.state.dataPost}
+    newdataPost['id'] = new Date().getTime()
+    newdataPost[e.target.name] = e.target.value
+    this.setState({
+      dataPost : newdataPost
+    }, () => console.log(this.state.dataPost))
+  }
+
+  postData = () => {
+    // axios.post(`http://localhost:3005/posts`, this.state.dataPost)
+    //   .then(res  => { this.handleReload()})
+    console.log("add data");
+    axios({
+      method : "POST",
+      url : "http://localhost:3005/posts",
+      data : this.state.dataPost
+    }).then(res  => { this.handleReload()})
   }
 
   componentDidMount(){
+
     // fetch("https://jsonplaceholder.typicode.com/posts")
     //   .then(response => response.json())
     //   .then(res => {
     //     this.setState({ dataApi : res })
     //     console.log(res);
     //   })
+
     this.handleReload()
       
 
@@ -36,17 +66,16 @@ class App extends Component {
     return (
       <div>
         <h3>React - API</h3>
+          <input type="text" name="title" placeholder="Masukan Title" onChange={this.handleChange}></input>
+          <input type="text" name="body" placeholder="Masukan Body" onChange={this.handleChange}></input>
+          <button type="submit" onClick={this.postData}>Add Data</button>
         {this.state.dataApi.map((dat, index)=>{
           return (
             <div key={index}>
-              {/* <ul>
-                <li > */}
-                  <p>{dat.id} : {dat.title} </p>
+                  {/* <p>{dat.id} : {dat.title} </p> */}
                   <p>{dat.title}</p>
+                  <p>{dat.body}</p>
                   <button value={dat.id} onClick={this.handleRemove}>Hapus Item</button>
-
-                {/* </li>
-              </ul> */}
             </div>
           )
         })}
